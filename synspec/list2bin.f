1fe4f1eed108d94f25bb6d69dd7d99c17503291c
Carlos <callende@iac.es>
2020-08-02 17:43:41 +0100

    improvements in list2bin and in the makefiles for synspec and linelist

--- synspec/list2bin.f
+++ synspec/list2bin.f
@@ -4,9 +4,22 @@ c
 c     transforms an ASCII line list to a binary
 c
       implicit real*8 (a-h,o-z)
+      character*80 infile,outfile
 c
       n=0
-      read(5,*) itype
+      read(*,*) infile
+      i = len_trim(infile)
+      if (infile(i-1:i) .eq. '19') then
+          itype = 1
+      else
+          itype = 2
+      endif
+
+      outfile = infile
+      outfile(i-1:i) = '11'
+      write(*,*) 'reading/writing =',infile, '/', outfile
+      open(19,file=infile,status='old')
+      open(11,file=outfile,status='new',form='unformatted')
 c
 c     itype: type of the line list
 c          = 0 - classical atomic line list
@@ -22,7 +35,7 @@ c
    20    continue
        else if(itype.eq.1) then
    30    continue
-         read(19,*,err=30,end=40) alam,anum,gf,excl,ql,excu,qu,i
+         read(19,*,err=30,end=40) alam,anum,gf,excl,ql,excu,qu,
      *        gr,gs,gw,i1,i2,i3,i4,i5,i6,i7
          write(11) alam,anum,gf,excl,ql,excu,qu,gr,gs,gw,
      *        i1,i2,i3,i4,i5,i6,i7
@@ -32,13 +45,13 @@ c
 c
        else
    50    continue
-         read(20,*,err=50,end=60) alam,anum,gf,excl,gr,gs,gw
+         read(19,*,err=50,end=60) alam,anum,gf,excl,gr,gs,gw
          write(11) alam,anum,gf,excl,gr,gs,gw
          n=n+1
          go to 50
    60    continue
       end if
 c
-      write(6,601) n
-  601 format(i10,'  lines included in the binary file fort.11')
+      write(6,601) n, outfile
+  601 format(i10,'  lines included in the binary file ',a80)
       end
