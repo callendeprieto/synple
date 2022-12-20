f6deffe4d7283dd6d032a14824750db59f9ad0be
Carlos Allende <callende@iac.es>
2022-12-20 00:15:29 +0000

    updated the dir tree (moving synple.py to a folder python/synple) and setup.py to enable the package installation

--- setup.py
+++ setup.py
@@ -1,51 +1,98 @@
-#!/usr/bin/env python
-
 import os
+import sys
 import shutil
-from setuptools import setup, find_packages, extension
+from setuptools import setup, find_packages, find_namespace_packages
 from setuptools.command.install import install
-#from numpy.distutils.core import setup, Extension
+import subprocess
 
+def get_virtualenv_path():
+    """Used to work out path to install compiled binaries to."""
+    if hasattr(sys, 'real_prefix'):
+        return sys.prefix
 
-class PreInstallCommand(install):
-        """Pre-installation for installation mode."""
-        def run(self):
-                # Step 1. Compile fortran code
-                os.system('cd synspec; make clean; make; cd ..')
-                # Step 2. Download the linelist
-                
-                
-class PostInstallCommand(install):
-        """Post-installation for installation mode."""
-        def run(self):
-                # Step 3. Copy binaries to scripts directory
-                
-                synspec = 'bin/synspec54'
-                if os.path.exists(synspec):
-                        self.copy_file(synspec,self.install_scripts)
-                rotin = 'bin/rotin'
-                if os.path.exists(rotin):
-                        self.copy_file(rotin,self.install_scripts)
-                
+    if hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix:
+        return sys.prefix
 
-#mod = extension.Extension(name = 'synspec', sources = ['synspec/synspec54.f'])
+    if 'conda' in sys.prefix:
+        return sys.prefix
+
+    return None
+
+def get_bin_path():
+    # Get environment bin/ directory
+    bindir = None
+    # check if the --user option was set
+    userdir = None
+    for u in install.user_options:
+        if u[0]=='user':
+            # "install in user site-package '/Users/nidever/.local/lib/python3.7/site-packages'"
+            uline = u[2]
+            userdir = uline[uline.find('site-package')+13:]
+            userdir = userdir.replace("'","")
+    if userdir is not None:
+        # /Users/nidever/.local/bin
+        bindir = os.path.dirname(os.path.dirname(os.path.dirname(userdir)))+'/bin'
+        if os.path.exists(bindir)==False:
+            bindir = False
+    # Try virtual environment using sys.prefix
+    if bindir is None:
+        venv = get_virtualenv_path()
+        if venv is not None:
+            bindir = venv+'/bin'
+            if os.path.exists(bindir)==False:
+                bindir = None
+    # Get bin/ directory from python executable
+    if bindir is None:
+        out = subprocess.check_output('which python',shell=True)
+        if type(out) is bytes:
+            out = out.decode()
+        bindir = os.path.dirname(out)
+        if os.path.exists(bindir)==False:
+            bindir = None
+
+    if bindir is None:
+        raise Exception('No bin/ directory found')
+
+    return bindir
+    
+def compile_and_install_software():
+    """Used the subprocess module to compile/install the C software."""
+    src_path = './synspec/'
+    
+    # Install the software
+    subprocess.check_call('make install', cwd=src_path, shell=True)
+
+    # Get the path for the binaries
+    bindir = get_bin_path()
+        
+    # Copy fortran binaries to bin/ directory
+    for f in ['rotin','synspec54','list2bin']:
+        if os.path.exists(bindir+f): os.remove(bindir+f)
+        print('Copying bin/'+f+' -> '+bindir+'/'+f)
+        shutil.copyfile('bin/'+f,bindir+'/'+f)
+        
+    # Download and convert linelists
+    curdir = os.path.abspath(os.curdir)
+    subprocess.check_call('make', cwd=curdir+'/linelists', shell=True)    
+    
+class CustomInstall(install):
+    """Custom handler for the 'install' command."""
+    def run(self):
+        compile_and_install_software()
+        super().run()
+
 setup(name='synple',
-      version='1.2',
+      version='1.0',
       description='An Easy-to-Use Python Wrapper for the Spectral Synthesis Code Synspec',
       author='Carlos Allende Prieto',
       author_email='callende@iac.es',
       url='https://github.com/callendeprieto/synple',
-      packages=find_packages(),
-      classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-      ],
-      python_requires='>=3.6',
-      #ext_modules = [mod],
       scripts=['bin/synple'],
-      requires=['gfortran','wget','numpy','astropy(>=4.0)','scipy'],
+      requires=['numpy','astropy(>=4.0)','scipy'],
+      cmdclass={'install': CustomInstall},
+      zip_safe = False,
       include_package_data=True,
-      #cmdclass={'install': PostInstallCommand},
-      #package_data={'synple': ['data/']},
+      packages=find_namespace_packages(where="python"),
+      package_dir={"": "python"}      
 )
+
