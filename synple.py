fc9075a994eb23c83710d7738e1704b7ba8dfb3c
Carlos Allende <callende@iac.es>
2023-10-07 13:14:28 +0100

    changed neighbors to 500 (RBFInterpolator)

--- synple.py
+++ synple.py
@@ -4438,7 +4438,7 @@ def rbf_get(synthfile, kernel='thin_plate_spline'):
   c = []
   for i in np.arange(nfreq):
     print('freq i / nfreq=',i,nfreq)
-    c.append(RBFInterpolator(iarr, d [:, i], kernel=kernel, neighbors = 500 ))
+    c.append(RBFInterpolator(iarr, d [:, i], kernel=kernel, neighbors = 200 ))
 
   return(c)
 
