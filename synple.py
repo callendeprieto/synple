0843539684c7cd3e3ded9f9db81ecafd30174744
Carlos Allende <callende@iac.es>
2023-10-07 11:05:33 +0100

    added the *neighbors* keyword in calling RBFInterpolator, limiting to 1e3 the input data points

--- synple.py
+++ synple.py
@@ -4438,7 +4438,7 @@ def rbf_get(synthfile, kernel='thin_plate_spline'):
   c = []
   for i in np.arange(nfreq):
     print('freq i / nfreq=',i,nfreq)
-    c.append(RBFInterpolator(iarr, d [:, i], kernel=kernel ))
+    c.append(RBFInterpolator(iarr, d [:, i], kernel=kernel, neighbors = 1000 ))
 
   return(c)
 
