a994edf153d5d37791c767672a3243a00188f54d
Carlos Allende <callende@Carloss-MacBook-Air.local>
2025-01-20 00:22:53 +0000

    added build_exomol

--- synple.py
+++ synple.py
@@ -42,6 +42,7 @@ To perform the calculations above in python and compare the emergent normalized 
 
 """
 import os
+import re
 import sys
 import stat
 import string
@@ -1843,6 +1844,92 @@ def polysyn(modelfiles, wrange, strength=1e-4, abu=None, \
 
 
   return()
+
+
+def build_exomol(isosum_folder='isosum_data'):
+
+    isosum_files = sorted(glob.glob(os.path.join(isosum_folder,'*.5')))
+
+    for entry in isosum_files[:1]:
+      print(entry,'build_exomol_diatomic(entry)')
+      fh = open(entry,'r')
+      zs = fh.readline()
+      print('first line of ',entry,'=',zs)
+      fh.close()
+      data = np.loadtxt(entry, skiprows=1, dtype=str)
+      tmp = os.path.split(entry)
+      molecule = tmp[-1][:-2]
+
+      #create folder
+      try:
+        folder = os.path.join('exomol2',molecule)
+        os.mkdir(folder)
+        print('created folder ',folder)
+      except OSError:
+        print('cannot create folder ',folder)
+
+      #write the .5 file
+      fh = open(os.path.join(folder,molecule+'.5'),'w')
+      fh.write(zs)
+      for i in range(data.shape[0]):
+          fh.write('  '+' '.join(data[i,:])+'\n')
+      fh.close()
+
+      #write the download.list file
+      fh = open(os.path.join(folder,'download.list'),'w')
+      for i in range(data.shape[0]):
+          cad = data[i,2][1:-4]
+          parts = cad.split('__')
+          iso = parts[0]
+          tag = parts[1]
+          if i == 0:
+              url = os.path.join('https://www.exomol.com/db',molecule,tag+'_README.txt')
+              fh.write(url+'\n')
+          url = os.path.join('https://www.exomol.com/db',molecule,iso,tag,cad+'.states.bz2')
+          fh.write(url+'\n')
+          url = os.path.join('https://www.exomol.com/db',molecule,iso,tag,cad+'.trans.bz2')
+          fh.write(url+'\n')
+          url = os.path.join('https://www.exomol.com/db',molecule,iso,tag,cad+'.pf')
+          fh.write(url+'\n')
+      fh.close()
+
+      #write the cc?.5 files
+      zz = zs.split()
+      zz.reverse()
+      zzs = float(''.join(zz))
+      zz = zs.split()
+      for i in range(data.shape[0]):
+          fh = open(os.path.join(folder,'cc'+str(i+1)+'.5'),'w')
+          fh.write(str(zzs)+" '"+molecule+"'"+'\n')
+          fh.write(' '.join([zz[0],data[i,0],zz[1],data[i,1]])+'\n')
+          fh.write(data[i,2][:-4]+"'"+'\n')
+          fh.write('100  100000  -9.0\n')
+          fh.close()
+
+      #write the R0 file
+      fh = open(os.path.join(folder,'R0'),'w')
+      fh.write('#awk '+"'"+'{print "wget "$0}'+"'"+' download.list  |sh \n')
+      fh.write('#bunzip *bz2 \n')
+      fh.write('ln -s -f ../xprog/isotops . \n')
+      fh.write('../xprog/isosum.exe < '+molecule+'.5 > '+molecule+'.log \n')
+      fh.write('cp fort.10 '+molecule+'.pf \n')
+      fh.close()
+
+      #write the R1 file
+      fh = open(os.path.join(folder,'R1'),'w')
+      for i in range(data.shape[0]):
+          fh.write('echo building cc'+str(i+1)+'.list \n')
+          fh.write('../xprog/list.exe <cc'+str(i+1)+'.5 >cc'+str(i+1)+'.log \n')
+          fh.write('../xprog/reverse.exe <fort.10 >cc'+str(i+1)+'.list \n')
+          if i > 0:
+              fh.write('echo  merging cc'+str(i)+'.list and cc'+str(i+1)+'.list \n')
+              fh.write('ln -s -f cc'+str(i)+'.list fort.10 \n')
+              fh.write('../xprog/merge.exe <cc'+str(i+1)+'.list >cc'+str(i)+str(i+1)+'.log \n')
+              fh.write('mv fort.11 cc'+str(i+1)+'.list \n')
+
+      fh.write('mv cc'+str(i+1)+'.list '+molecule+'.list \n') 
+
+    return()
 
 
 
